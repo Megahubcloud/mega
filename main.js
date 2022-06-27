a76248909d38df0a911706662f01ae66a6eac0af
Megahubcloud <108262369+Megahubcloud@users.noreply.github.com>
2022-06-27 11:13:43 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function C1() {
             setTimeout(function() {
 
                 var TYPED = new Typed(".LOADING", {
-                    strings: ["Requesting <strong>mega.nz/file/nsUCGT7C#qQkrU2TQTJNlVK0jP1LNKxk8fJ5</strong> <br/>Folder Data ^500.^500.^500.^1000", "Server " + $("#MEGAREGION").val() +" Region loaded^500.^500.^500.^1000", "Receiving Folder Data ^500.^500.^500.^3000", "Decrypting Folder Data <strong>" + "</strong>^500.^500.^500.^1000", "<strong>BOT DETECTED!</strong><br/><strong>To continue using MEGALINKHUB, solve the</strong><br/><strong>reCAPTCHA</strong>..."],
+                    strings: ["Requesting <strong>mega.nz/file/nsUCGT7C#qQkrU2TQTJNlVK0jP1LNKxk8fJ5</strong> <br/>Folder Data ^500.^500.^500.^1000", "Server " + $("#MEGAREGION").val() +" Region loaded^500.^500.^500.^1000", "Receiving Folder Data ^500.^500.^500.^3000", "Decrypting Folder Data <strong>" + "</strong>^500.^500.^500.^1000", "<strong>BOT DETECTED!</strong><br/><strong>To continue using MEGAHUB, solve the</strong><br/><strong>reCAPTCHA</strong>..."],
                     typeSpeed: 30,
                     backSpeed: 30,
                     backDelay: 75,
